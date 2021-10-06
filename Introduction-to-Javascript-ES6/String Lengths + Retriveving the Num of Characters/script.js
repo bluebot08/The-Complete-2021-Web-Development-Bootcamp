0ffa2ad358d9091a2c5abeae2a0c0d8076e53d7b
Stefan O'Neil <oneil.stefan@gmail.com>
2021-10-06 14:17:39 +0900

    added script.

--- Introduction-to-Javascript-ES6/String Lengths + Retriveving the Num of Characters/script.js
+++ Introduction-to-Javascript-ES6/String Lengths + Retriveving the Num of Characters/script.js
@@ -1,1 +1,21 @@
+var characterLimit = 140;
+var messageLength;
+var remainingCharacters;
 
+alert(
+  "This will count how many characters your message has and the remaining characters left based on a 140 character limit"
+);
+
+var yourMessage = prompt("Please enter your message");
+
+messageLength = yourMessage.length;
+
+remainingCharacters = characterLimit - messageLength;
+
+alert(
+  "Your message contains a total of " +
+    messageLength +
+    " characters. You have " +
+    remainingCharacters +
+    " characters left."
+);
