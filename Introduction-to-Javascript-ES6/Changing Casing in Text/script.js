e8642ce6a20a17fdae608a3a307b957ec8bfbe35
Stefan O'Neil <oneil.stefan@gmail.com>
2021-10-07 15:32:06 +0900

    script to only uppercase first letter in name.

--- Introduction-to-Javascript-ES6/Changing Casing in Text/script.js
+++ Introduction-to-Javascript-ES6/Changing Casing in Text/script.js
@@ -1,1 +1,7 @@
+let userName = prompt("What is your name?");
 
+let firstLetter = userName.slice(0, 1);
+let restName = userName.slice(1, 99);
+firstLetter = firstLetter.toUpperCase();
+
+alert("Hello, " + firstLetter + restName);
