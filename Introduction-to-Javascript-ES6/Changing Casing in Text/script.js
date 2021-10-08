2c203c866741ed1322d407cee2eeb80690228551
Stefan O'Neil <oneil.stefan@gmail.com>
2021-10-08 11:36:11 +0900

    more elegant solution.

--- Introduction-to-Javascript-ES6/Changing Casing in Text/script.js
+++ Introduction-to-Javascript-ES6/Changing Casing in Text/script.js
@@ -1,7 +1,7 @@
 let userName = prompt("What is your name?");
 
 let firstLetter = userName.slice(0, 1);
-let restName = userName.slice(1, 99);
+let restName = userName.slice(1, userName.length);
 firstLetter = firstLetter.toUpperCase();
 
 alert("Hello, " + firstLetter + restName + ".");
