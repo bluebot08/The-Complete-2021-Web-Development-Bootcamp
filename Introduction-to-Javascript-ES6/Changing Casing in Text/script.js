97be330c85bfab39a421b7e3d1bd60c0c1d66e46
Stefan O'Neil <oneil.stefan@gmail.com>
2021-10-08 11:39:39 +0900

    code to change rest of name to lowercase.

--- Introduction-to-Javascript-ES6/Changing Casing in Text/script.js
+++ Introduction-to-Javascript-ES6/Changing Casing in Text/script.js
@@ -3,5 +3,6 @@ let userName = prompt("What is your name?");
 let firstLetter = userName.slice(0, 1);
 let restName = userName.slice(1, userName.length);
 firstLetter = firstLetter.toUpperCase();
+restName = restName.toLocaleLowerCase();
 
 alert("Hello, " + firstLetter + restName + ".");
