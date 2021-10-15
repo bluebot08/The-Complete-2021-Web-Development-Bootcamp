e5646468e8de4e650665939df766a80720caf050
Stefan O'Neil <oneil.stefan@gmail.com>
2021-10-15 13:54:33 +0900

    finished project.

--- Introduction-to-Javascript-ES6/Functions Part 2/script.js
+++ Introduction-to-Javascript-ES6/Functions Part 2/script.js
@@ -1,4 +1,7 @@
-function getMilk() {
+var money = prompt("How much money will you give the robot?");
+var bottlesBought = Math.floor(money / 1.5);
+
+function getMilk(money) {
   console.log("leaveHouse");
   console.log("moveRight");
   console.log("moveRight");
@@ -8,6 +11,7 @@ function getMilk() {
   console.log("moveUp");
   console.log("moveRight");
   console.log("moveRight");
+  console.log("buy " + bottlesBought + " bottles of Milk");
   console.log("moveLeft");
   console.log("moveLeft");
   console.log("moveDown");
@@ -18,3 +22,7 @@ function getMilk() {
   console.log("moveLeft");
   console.log("enterHouse");
 }
+
+getMilk(money);
+
+alert(`The robot bought ${bottlesBought} bottles of Milk`);
