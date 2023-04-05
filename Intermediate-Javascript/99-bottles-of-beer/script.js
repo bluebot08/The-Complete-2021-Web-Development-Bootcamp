9f46708f35a376cd4279375ea477f04c1212d630
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-06 04:47:15 +0900

    refactored code

--- Intermediate-Javascript/99-bottles-of-beer/script.js
+++ Intermediate-Javascript/99-bottles-of-beer/script.js
@@ -1,16 +1,31 @@
 function bottlesOfBeer() {
   let count = 99;
-  while (count <= 99 && count >= 1) {
+  while (count >= 1) {
     console.log(
-      count + " bottles of beer on the wall, " + count + " bottles of beer."
+      `${count} bottles of beer on the wall, ${count} bottles of beer.`
     );
     count--;
     console.log(
-      "Take one down and pass it around, " +
-        count +
-        " bottles of beer on the wall."
+      `Take one down and pass it around, ${count} bottles of beer on the wall.`
     );
   }
 }
 
 bottlesOfBeer();
+
+// function bottlesOfBeer() {
+//     let count = 99;
+//     while (count >= 1) {
+//       console.log(
+//         count + " bottles of beer on the wall, " + count + " bottles of beer."
+//       );
+//       count--;
+//       console.log(
+//         "Take one down and pass it around, " +
+//           count +
+//           " bottles of beer on the wall."
+//       );
+//     }
+//   }
+
+//   bottlesOfBeer();
