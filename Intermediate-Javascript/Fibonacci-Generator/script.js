4ba743f2e4b908818e939ea28fe139d1af449342
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-07 00:13:06 +0900

    Added Yu solution.

--- Intermediate-Javascript/Fibonacci-Generator/script.js
+++ Intermediate-Javascript/Fibonacci-Generator/script.js
@@ -36,3 +36,22 @@ fibGen();
 //   }
 //   return fibSeq
 // }
+
+// Angela Yu solution
+
+// function fibonacciGenerator (n){
+//   var output = []
+//   if ( n === 1) {
+//     output = [0];
+//   }
+//   else if ( n === 2 ) {
+//     output = [0,1];
+//   }
+//   else {
+//     output = [0,1];
+//     for (var i = 2; i < 2; i++) {
+//       output.push(output[output.length -2 ] + output.[output.length -1])
+//     }
+//   }
+//   return output
+// }
