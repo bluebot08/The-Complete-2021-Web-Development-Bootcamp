c2095693c2c1ea8bc218ac1f21d7fc7d4653096e
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-06 21:19:35 +0900

    Added array

--- Intermediate-Javascript/Fibonacci-Generator/script.js
+++ Intermediate-Javascript/Fibonacci-Generator/script.js
@@ -3,8 +3,11 @@ let fibonacciArray = [];
 function fibonacciGenerator(n) {
   //Do NOT change any of the code above 👆
   //Write your code here:
+
+  fibonacciNumber = 
+
   //Return an array of fibonacci numbers starting from 0.
   //Do NOT change any of the code below 👇
 }
 
-fibonacciGenerator();
+fibonacciGenerator(3);
