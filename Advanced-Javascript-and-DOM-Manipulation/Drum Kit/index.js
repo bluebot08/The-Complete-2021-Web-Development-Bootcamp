c2d5e9a2ec9b6333c7d8d21b2f4fbb7094727f87
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-08 03:56:45 +0900

    Added this function.

--- Advanced-Javascript-and-DOM-Manipulation/Drum Kit/index.js
+++ Advanced-Javascript-and-DOM-Manipulation/Drum Kit/index.js
@@ -15,6 +15,9 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked");
+    this.style.color = "white";
   });
 }
+
+// var sound = new Audio("sounds/tom-1.mp3");
+// sound.play();
