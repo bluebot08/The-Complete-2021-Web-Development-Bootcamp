556cdf9742139c45fdd0703d9fc92f031cb88050
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-07 22:35:31 +0900

    Finished assignment

--- The-Document-Object-Model-DOM/Introduction-to-the-Document-Object-Model-DOM/scripts.js
+++ The-Document-Object-Model-DOM/Introduction-to-the-Document-Object-Model-DOM/scripts.js
@@ -1,3 +1,5 @@
 document.querySelectorAll(".list")[2].innerHTML = "Stefan O'Neil";
 
 document.querySelector(".list a").style.color = "red";
+
+document.querySelector(".btn").style.backgroundColor = "yellow";
