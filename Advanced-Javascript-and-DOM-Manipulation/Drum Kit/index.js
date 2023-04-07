28bf29376635d244b60e69fd39d1008c10b0d2ee
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-08 02:27:12 +0900

    Finished assignment

--- Advanced-Javascript-and-DOM-Manipulation/Drum Kit/index.js
+++ Advanced-Javascript-and-DOM-Manipulation/Drum Kit/index.js
@@ -1,5 +1,20 @@
-document.querySelector("button").addEventListener("click", handleClick);
+// document.querySelector("button").addEventListener("click", function () {
+//   alert("I got clicked!");
+// });
 
-function handleClick() {
-  alert("I got clicked!");
+// My solution
+// let buttons = document.querySelectorAll("button");
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", function () {
+//     alert("I got clicked!");
+//   });
+// });
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked");
+  });
 }
