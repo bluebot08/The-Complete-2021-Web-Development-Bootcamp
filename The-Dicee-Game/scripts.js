d7dbfff8613752c19459ecd8cb938abdd2c6298e
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-08 00:04:02 +0900

    Finished assignment

--- The-Dicee-Game/scripts.js
+++ The-Dicee-Game/scripts.js
@@ -40,6 +40,16 @@ function randomNumGen() {
     document.querySelector(".img2").setAttribute("src", "images/dice6.png");
   }
 
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  }
+  if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  }
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+
   console.log(randomNumber1);
   console.log(randomNumber2);
 }
