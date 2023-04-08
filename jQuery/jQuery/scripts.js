424622c7aa23f4eeeb726c3fe3ec1357a0bf3101
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-09 04:46:48 +0900

    Finished assignment

--- jQuery/jQuery/scripts.js
+++ jQuery/jQuery/scripts.js
@@ -1,5 +1,7 @@
 // $("h1").css("color", "red");
 
 $("h1").addClass("big-title margin-50");
+$("h1").text("Good Bye!");
 
-$("button");
+// $("button").text("Don't Click Me!");
+$("button").html("<em>Don't Click Me!</em>");
