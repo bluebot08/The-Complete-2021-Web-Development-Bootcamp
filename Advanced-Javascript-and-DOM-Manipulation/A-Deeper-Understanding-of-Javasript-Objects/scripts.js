376f7c53c9f7d0c6d7a035ec6fa614afe3a75878
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-09 01:35:01 +0900

    Added function to object.

--- Advanced-Javascript-and-DOM-Manipulation/A-Deeper-Understanding-of-Javasript-Objects/scripts.js
+++ Advanced-Javascript-and-DOM-Manipulation/A-Deeper-Understanding-of-Javasript-Objects/scripts.js
@@ -8,6 +8,11 @@ function HouseKeeper(yearsOfExperience, name, cleaningRepertoire) {
   this.yearsOfExperience = yearsOfExperience;
   this.name = name;
   this.cleaningRepertoire = cleaningRepertoire;
+  this.clean = function () {
+    alert("Cleaning in progress...");
+  };
 }
+
+// How to create object using object constructor
 
 // var housekeeper1 = new HouseKeeper(5, "Kate", ["lobby", "bedroom"]);
