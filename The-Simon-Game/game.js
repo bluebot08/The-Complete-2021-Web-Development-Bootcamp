4cdcf5ec4cb2d41f69ba60097682993d16808e8c
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-09 21:31:45 +0900

    Added flashing animation for button

--- The-Simon-Game/game.js
+++ The-Simon-Game/game.js
@@ -1,14 +1,26 @@
 // alert("This is a test");
 
+// Create arrays
+
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 
 function nextSequence() {
+  // Generate random number
   let randomNumber = Math.floor(Math.random() * 4);
+
+  //   Use number to choose color and push it to gamePatter array
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
   console.log(randomNumber);
   console.log(randomChosenColor);
+
+  //   flashing button animation
+  let selectedButton = $("." + randomChosenColor);
+  $(selectedButton).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  console.log(selectedButton);
+
+  //   create audio and play
 }
 
 nextSequence();
