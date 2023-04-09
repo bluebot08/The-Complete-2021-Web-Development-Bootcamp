182fcd65cbfcb523b2d0b75d89611700c9b602b1
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-10 04:41:46 +0900

    Added supervillains npm

--- Node/index.js
+++ Node/index.js
@@ -1,5 +1,17 @@
-console.log("I did it!");
+// console.log("I did it!");
 
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.copyFileSync("file1.txt", "file2.txt");
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+var superheroes = require("superheroes");
+
+var mySuperHeroName = superheroes.random();
+
+console.log("My superhero name is " + mySuperHeroName);
+
+const supervillains = require("supervillains");
+
+var mySuperVillainName = supervillains.random();
+
+console.log("My supervillain name is " + mySuperVillainName);
