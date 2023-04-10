71beea9c8761ca0ec0245ae39095e453a0ab4322
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-10 22:52:12 +0900

    added response for server

--- Express-js/my-express-server/server.js
+++ Express-js/my-express-server/server.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express();
 
+// app.get("/", function (request, response) {
+//   //   console.log(request);
+//   //   response.send("Hello");
+//   response.send("<h1>Hello, World!</h1>");
+// });
+
+// Most use req and res instead of request and response
+
+app.get("/", function (req, res) {
+  //   console.log(request);
+  //   response.send("Hello");
+  response.send("<h1>Hello, World!</h1>");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
