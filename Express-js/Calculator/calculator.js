7c58a120499afef57a36e107d3336eb705a193a3
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-11 01:23:54 +0900

    finished assignment

--- Express-js/Calculator/calculator.js
+++ Express-js/Calculator/calculator.js
@@ -14,9 +14,18 @@ app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
 
+// Sends index.html
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
+// Sends bmiCalculator.html
+app.get("/bmiCalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+// Calculating math problem
 
 app.post("/", function (req, res) {
   //   console.log(req.body.num1);
@@ -27,10 +36,22 @@ app.post("/", function (req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
 
-  //   calulation
+  //   calculation
 
   var result = num1 + num2;
   res.send(
     "The result of adding " + num1 + " and " + num2 + " is " + result + "."
   );
+});
+
+// Calculating BMI
+
+app.post("/bmiCalculator", function (req, res) {
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+
+  //   calculation
+
+  var bmiResult = Math.round(weight / (height * height));
+  res.send("Your BMI is " + bmiResult);
 });
