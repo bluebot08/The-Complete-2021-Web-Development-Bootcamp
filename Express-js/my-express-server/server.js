1d3e91525292b040ed614be222adba8ae2c5d094
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-10 23:16:58 +0900

    installed

--- Express-js/my-express-server/server.js
+++ Express-js/my-express-server/server.js
@@ -20,6 +20,16 @@ app.get("/contact", function (req, res) {
   res.send("Contact me at: oneil.stefan@gmail.com");
 });
 
+app.get("/about", function (req, res) {
+  res.send(
+    "Hi, I'm Stefan! After taking a year-long break to help raise my child 👶🏼, I'm excited to be back and ready to dive into my passions for movies 🎥, music 🎵, games 🎮, and technology 💻. I'm particularly interested in creating websites that enhance people's enjoyment of these areas. If you want to reach me, the best way is to email me at oneil.stefan@gmail.com I'm looking forward to connecting with you!"
+  );
+});
+
+applget("/hobbies", function (req, res) {
+  res.send("I love movies!");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
