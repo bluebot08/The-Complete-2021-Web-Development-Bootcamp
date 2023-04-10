c89aecc0f00ad47287d3804125445efffd85498e
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-10 23:01:00 +0900

    added contact response

--- Express-js/my-express-server/server.js
+++ Express-js/my-express-server/server.js
@@ -16,6 +16,10 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello, World!</h1>");
 });
 
+app.get("/contact", function (req, res) {
+  res.send("Contact me at: oneil.stefan@gmail.com");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
