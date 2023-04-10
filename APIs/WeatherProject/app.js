99b18d3c9da2cb50c920aa65d87083752de1ca4c
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-11 04:32:12 +0900

    Finished assignment

--- APIs/WeatherProject/app.js
+++ APIs/WeatherProject/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 
+const https = require("https");
+
 // initialze express
 
 const app = express();
 
 app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Miura&appid=5ed0351a230c2fc8207ce933d27fd7be&units=metric";
+
+  https.get(url, function (response) {
+    console.log(response);
+  });
+
   res.send("Server is up and running");
 });
 
