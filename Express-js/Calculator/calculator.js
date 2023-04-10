acd15726689e3ccaeae334daf234252348857a22
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-10 23:36:47 +0900

    Added response on home

--- Express-js/Calculator/calculator.js
+++ Express-js/Calculator/calculator.js
@@ -5,3 +5,9 @@ const app = express();
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+
+app.get("/", function (req, res) {
+  res.send(
+    "<h1>Hello, World! This is the home of the calculator web app!</h1>"
+  );
+});
