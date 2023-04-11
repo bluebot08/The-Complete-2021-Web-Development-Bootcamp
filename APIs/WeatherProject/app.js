b2c1346b230329aabe55dc6a8927388d5ecec1c5
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-11 21:47:13 +0900

    sending index.html

--- APIs/WeatherProject/app.js
+++ APIs/WeatherProject/app.js
@@ -8,70 +8,58 @@ const https = require("https");
 const app = express();
 
 app.get("/", function (req, res) {
-  const query = "Tokyo";
-  const apiKey = "5ed0351a230c2fc8207ce933d27fd7be";
-  const unit = "metric";
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    query +
-    "&appid=" +
-    apiKey +
-    "&units=" +
-    unit;
-
-  https.get(url, function (response) {
-    // logging response code to see if it things are working
-    console.log(response.statusCode);
-
-    // saving the data
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      console.log(weatherData);
-
-      //   Saving specific data
-      const temp = weatherData.main.temp;
-      const feelsLikeTemp = weatherData.main.feels_like;
-      const descriptionSky = weatherData.weather[0].description;
-      const description = weatherData.weather[0].main;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-      //   Responding
-      res.write(
-        "<p>The weather is currently " +
-          description +
-          " with " +
-          descriptionSky +
-          " </p>"
-      );
-      res.write(
-        "<h1>The temperature in Miura is " +
-          temp +
-          " and feels like " +
-          feelsLikeTemp +
-          " degrees Celcius</h1>"
-      );
-      res.write("<img src=" + imageURL + ">");
-
-      res.send();
-
-      //   My original way
-      //   res.send(
-      //     "<h1>The temperature in Miura is " +
-      //       temp +
-      //       " degrees Celcius. The feels like temperature is " +
-      //       feelsLikeTemp +
-      //       ". The weather is currently " +
-      //       description +
-      //       " with " +
-      //       descriptionSky +
-      //       " .</h1>"
-      //   );
-    });
-  });
-  // Need to remove because we can only have one res
-  //   res.send("Server is up and running");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
 });
+
+// const query = "Tokyo";
+// const apiKey = "5ed0351a230c2fc8207ce933d27fd7be";
+// const unit = "metric";
+// const url =
+//   "https://api.openweathermap.org/data/2.5/weather?q=" +
+//   query +
+//   "&appid=" +
+//   apiKey +
+//   "&units=" +
+//   unit;
+
+// https.get(url, function (response) {
+//   // logging response code to see if it things are working
+//   console.log(response.statusCode);
+
+//   // saving the data
+//   response.on("data", function (data) {
+//     const weatherData = JSON.parse(data);
+//     console.log(weatherData);
+
+//     //   Saving specific data
+//     const temp = weatherData.main.temp;
+//     const feelsLikeTemp = weatherData.main.feels_like;
+//     const descriptionSky = weatherData.weather[0].description;
+//     const description = weatherData.weather[0].main;
+//     const icon = weatherData.weather[0].icon;
+//     const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+//     //   Responding
+//     res.write(
+//       "<p>The weather is currently " +
+//         description +
+//         " with " +
+//         descriptionSky +
+//         " </p>"
+//     );
+//     res.write(
+//       "<h1>The temperature in Miura is " +
+//         temp +
+//         " and feels like " +
+//         feelsLikeTemp +
+//         " degrees Celcius</h1>"
+//     );
+//     res.write("<img src=" + imageURL + ">");
+
+//     res.send();
+
+//   });
+// });
