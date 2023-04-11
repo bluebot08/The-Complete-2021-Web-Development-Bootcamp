16e31eaf4694a73a343da94c20f62f75dcfb9e48
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-11 21:51:12 +0900

    added post method

--- APIs/WeatherProject/app.js
+++ APIs/WeatherProject/app.js
@@ -11,6 +11,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function (req, res) {
+  console.log("Post request recieved");
+});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
 });
