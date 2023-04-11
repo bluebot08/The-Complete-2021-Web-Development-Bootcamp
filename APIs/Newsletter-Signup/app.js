a776f02e74fce4c7afd7e30a88bd805b6b6fa34e
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-11 22:24:35 +0900

    Setup modules and init server

---
+++ APIs/Newsletter-Signup/app.js
@@ -0,0 +1,19 @@
+// Install Modules
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+// initialze express
+
+const app = express();
+
+// initialze body-parser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set express to listen to port 3000 with message
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000.");
+});
