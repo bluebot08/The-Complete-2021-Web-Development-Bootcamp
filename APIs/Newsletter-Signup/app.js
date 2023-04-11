3a83845bcda2092ac9515518584cf89c97e3ed01
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-11 23:16:30 +0900

    added get function to serve signup.html

--- APIs/Newsletter-Signup/app.js
+++ APIs/Newsletter-Signup/app.js
@@ -17,3 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
 });
+
+// Functions
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
